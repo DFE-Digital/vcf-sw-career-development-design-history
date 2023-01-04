14788ffcc2e8c64baa640ab2c68660c047bf89be
Jonathan Porton <jonathan@mubudigital.co.uk>
2023-01-04 16:40:06 +0000

    Updated service name and styling fixes at various viewports

--- app/_data/app.js
+++ app/_data/app.js
@@ -1,5 +1,5 @@
 module.exports = {
-  productName: 'Social Worker Career Development',
+  productName: 'Develop your career in child and family social work',
   description: 'A history of the designs for the Social Worker Career Development service',
   sections: {
     title: 'Designs'
